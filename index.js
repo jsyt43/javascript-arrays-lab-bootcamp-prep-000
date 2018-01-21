db4a721bdb3c8f210ea5524b874bb479c7f90fdf
Jennifer Tsai <jsyt.kai@gmail.com>
2018-01-21 19:18:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
