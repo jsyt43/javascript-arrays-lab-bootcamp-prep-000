cd9690e35dc4f7caab4281015474ff87d6fa2ee1
Jennifer Tsai <jsyt.kai@gmail.com>
2018-01-21 19:39:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  kittens = [kittens, ...name];
-  return kittens;
+  return [kittens, ...name];
 }
 
 function prependKitten(name) {
